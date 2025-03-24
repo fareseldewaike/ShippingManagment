2a4f7e2d61a9de7bc32ff3ce253db22023b2c38e
fareseldewaike <fareseldewaike@gmail.com>
2025-03-25 00:23:07 +0200

    lat

--- Final-project/Shipping/obj/Debug/net9.0/Shipping.AssemblyInfo.cs
+++ Final-project/Shipping/obj/Debug/net9.0/Shipping.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shipping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+976e5e130c1c79b38f3bec9ceb74f5a125ac8375")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shipping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shipping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
