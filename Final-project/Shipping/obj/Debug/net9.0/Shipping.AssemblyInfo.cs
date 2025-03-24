aa226dea87a4689c898647f123df9b853ea02c44
fareseldewaike <fareseldewaike@gmail.com>
2025-03-25 01:13:44 +0200

    serg model

--- Final-project/Shipping/obj/Debug/net9.0/Shipping.AssemblyInfo.cs
+++ Final-project/Shipping/obj/Debug/net9.0/Shipping.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shipping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48d4141f1365f774a32913c8f43703aaa07a47d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9003a7b144e8254a0c9f180e8c81c2dfc7ca9cc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shipping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shipping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
