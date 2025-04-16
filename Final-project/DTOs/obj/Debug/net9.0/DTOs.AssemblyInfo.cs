b8c4b7da8b672fd03e13f59f0c1613df40409b9f
fareseldewaike <fareseldewaike@gmail.com>
2025-04-16 12:29:33 +0200

    vhjdh

--- Final-project/DTOs/obj/Debug/net9.0/DTOs.AssemblyInfo.cs
+++ Final-project/DTOs/obj/Debug/net9.0/DTOs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb2a3a3accfcc7837726f849963836345ae25c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f24e9db263a34b4384364a018e6a57f1efad09")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
